7a0f5a3a243836a997ccd7d3764c473385e0c00c
pncontemplacion <paulbcon@hotmail.com>
2019-05-17 13:22:23 -0400

    Creating Relationship

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
