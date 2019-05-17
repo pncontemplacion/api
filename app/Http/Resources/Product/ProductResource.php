3711d7088ea305fdb906ebc3471696011eb5c594
pncontemplacion <paulbcon@hotmail.com>
2019-05-17 14:53:24 -0400

    Product Collection Resource

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        setlocale(LC_MONETARY, 'en_US');
+        return [
+            'name' => $this->name,
+            'description' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'discountedPrice' => round((1-($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'Not yet rated',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
+        ];
+    }
+}
